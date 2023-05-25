ee87f9b71e22b0e0b6ff26de295ad65bc97c6ab2
giukix <SalvatoreGiurdanella@host.docker.internal>
2023-05-25 14:28:31 +0200

    Utilizzato completamente il pattern Observer

--- SwingExp/SwingExp/src/model/MyModel.java
+++ SwingExp/SwingExp/src/model/MyModel.java
@@ -1,21 +1,16 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
-import java.util.Observer;
 
 @SuppressWarnings("deprecation")
 public class MyModel extends Observable {
 	
 	private String myText;
 	private int numberOfInteractions;	
-	private List<Observer> observers;
 
 	public MyModel(String myText) {
 		this.myText = myText;
 		numberOfInteractions = 0;
-		observers = new ArrayList<>();
 	}
 
 	public String getMyText() {
@@ -25,26 +20,17 @@ public class MyModel extends Observable {
 	public void setMyText(String myText) {
 		this.myText = myText;
 		numberOfInteractions++;
+		setChanged();
 		notifyObservers();
 	}
 	
 	public void addText(String textToAdd) {		
 		myText += (myText.length() > 0 ? " + " : "") + textToAdd;
 		numberOfInteractions++;
+		setChanged();
 		notifyObservers();
 	}
 	
-	@Override
-	public synchronized void addObserver(Observer o) {
-		observers.add(o);		
-	}
-
-	@Override
-	public void notifyObservers() {
-		for(Observer o : observers)
-			o.update(null, o);
-	}
-
 	public int getNumberOfInteractions() {
 		return numberOfInteractions;
 	}
